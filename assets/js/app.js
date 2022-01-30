c0c605b212bd6e2349fb2444469af00bdc5cba7a
milan <pperisic.milan@gmail.com>
2022-01-30 14:40:54 +0100

    Add JS and changes in header

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,42 @@
 // buttons
-selectRealm = document.getElementById('select-realm')
-selectBlueEssence = document.getElementById('select-blue-essence')
+// selectRealm = document.getElementById('select-realm')
+// selectBlueEssence = document.getElementById('select-blue-essence')
+selectRealms = document.querySelectorAll('.select-realm')
+selectBlueEssences = document.querySelectorAll('.select-blue-essence')
 // sekcije
 chooseServer = document.getElementById('choose-server-section')
 chooseEssence = document.getElementById('choose-essence')
 checkoutSection = document.getElementById('checkout-section')
 
-selectRealm.onclick = function(){
-    chooseServer.style.display = "none"
-    chooseEssence.style.display = "block"
-}
+selectRealms.forEach(selectRealm => {
+    selectRealm.addEventListener('click', e => {
+      chooseServer.style.display = "none"
+      chooseEssence.style.display = "block"
+      console.log(e.target);
+    });
+});
 
-selectBlueEssence.onclick = function(){
-    chooseEssence.style.display = "none"
-    chooseServer.style.display = "none"
-    checkoutSection.style.display = "flex"
-}
+
+selectBlueEssences.forEach(selectBlueEssence => {
+    selectBlueEssence.addEventListener('click', e => {
+      chooseEssence.style.display = "none"
+      chooseServer.style.display = "none"
+      checkoutSection.style.display = "flex"
+      console.log(e.target);
+    });
+});
+
+
+// selectRealm.onclick = function(){
+//     chooseServer.style.display = "none"
+//     chooseEssence.style.display = "block"
+// }
+
+// selectBlueEssence.onclick = function(){
+//     chooseEssence.style.display = "none"
+//     chooseServer.style.display = "none"
+//     checkoutSection.style.display = "flex"
+// }
 // if checbox checked
 function checkboxFunction() {
     var checkBox = document.getElementById("checkbox-warranty")
@@ -29,3 +50,16 @@ function checkboxFunction() {
         price.innerHTML = "$11.00"
     }
   }
+
+  // circle pagination
+
+const numberButtons = document.querySelectorAll('.numbers-button');
+const numberButtonsArr = Array.from(numberButtons);
+numberButtonsArr.forEach(numberButton => {
+    numberButton.addEventListener('click', e => {
+        if(numberButton.classList.contains("button-active")){
+            console.log('test')
+        }
+      console.log(e.target);
+    });
+});
