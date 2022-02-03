6c378941844f14418004e414c49d34f2ffb34089
milan <pperisic.milan@gmail.com>
2022-02-03 23:50:13 +0100

    Back to top button

--- assets/js/app.js
+++ assets/js/app.js
@@ -91,3 +91,30 @@ numberThirdSecond.addEventListener('click', function(){ // iskljucivo radi u kor
   chooseEssence.style.display = "block"
   checkoutSection.style.display = "none"
 })
+
+// Back to top button
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
